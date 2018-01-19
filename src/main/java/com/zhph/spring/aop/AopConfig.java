915e1686162cd25ac1ab2a050fe054fa32bc3348
xianleii <strong_xian@163.com>
2018-01-19 13:27:40 +0800

    增加切面方法

--- src/main/java/com/zhph/spring/aop/AopConfig.java
+++ src/main/java/com/zhph/spring/aop/AopConfig.java
@@ -5,7 +5,7 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
 @Configurable
-@ComponentScan("com.zhph.spring.springboot.lesson1.aop")
+@ComponentScan("com.zhph.spring.aop.*")
 @EnableAspectJAutoProxy//开启Spring 对 Aspect的支持
 public class AopConfig {
 
